bab201176af346f291b22802b857d46cb42fc216
Patryk Wyprzał <patryk836@o2.pl>
2020-11-29 23:58:39 +0100

    Add unit of work.

---
+++ GymHelper/GymHelper/Data/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymHelper.Data.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepository<TEntity> Repository<TEntity>()
+            where TEntity : class;
+    }
+}
