9a3d428957acfe0e64dfd6b563a66555b1cb9beb
Patryk Wyprzał <patryk836@o2.pl>
2020-11-29 22:52:53 +0100

    Add generic repository.

---
+++ GymHelper/GymHelper/Data/Interfaces/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymHelper.Data.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<bool> Add(TEntity entity);
+        Task<bool> Delete(TEntity entity);
+        Task<List<TEntity>> ReadAll();
+        Task<List<TEntity>> ReadAllByCondition(Func<TEntity, bool> condition);
+        Task<List<TEntity>> ReadAllByConditionWithInclude<TProp>(Func<TEntity, bool> condition,
+            Expression<Func<TEntity, TProp>> include);
+        Task<TEntity> ReadFirstByCondition(Func<TEntity, bool> condition);
+        Task<TEntity> ReadFirstByConditionWithInclude<TProp>(Func<TEntity, bool> condition,
+            Expression<Func<TEntity, TProp>> include);
+        Task<bool> CheckIfExistByCondition(Func<TEntity, bool> condition);
+        Task<bool> Update(TEntity entity);
+    }
+}
