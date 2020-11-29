37e19f86d81fd87e11c3574e7ed083a5da991d5a
Patryk Wyprzał <patryk836@o2.pl>
2020-11-29 22:28:36 +0100

    Add data context.

---
+++ GymHelper/GymHelper/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymHelper.Data
+{
+    public class DataContext : DbContext
+    {
+        private readonly string dbPath;
+        public DataContext(string dbPath) : base()
+        {
+            this.dbPath = dbPath;
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Filename={dbPath}");
+        }
+    }
+}
