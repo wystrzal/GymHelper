b09fc2d010490014cb2b59abea9706b1fa1eda08
Patryk Wyprzał <patryk836@o2.pl>
2020-12-01 19:38:55 +0100

    Add base view model.

---
+++ GymHelper/GymHelper/ViewModel/BaseViewModel.cs
@@ -0,0 +1,36 @@
+﻿using GymHelper.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GymHelper.ViewModel
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async Task Navigate<TPage>()
+            where TPage : Page
+        {
+            await App.Current.MainPage.Navigation.PushAsync(Activator.CreateInstance(typeof(TPage)) as Page);
+        }
+
+        public async Task Navigate<TPage>(object providedObject)
+           where TPage : Page
+        {
+            await App.Current.MainPage.Navigation.PushAsync(Activator.CreateInstance(typeof(TPage), providedObject) as Page);
+        }
+
+        public async Task NavigateBack()
+        {
+            await App.Current.MainPage.Navigation.PopAsync();
+        }
+    }
+}
