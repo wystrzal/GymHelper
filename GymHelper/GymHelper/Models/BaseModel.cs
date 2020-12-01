23d4a3d5f05bf0465394835e60dad36f88b5f8c4
Patryk Wyprzał <patryk836@o2.pl>
2020-12-01 19:24:23 +0100

    Add user model.

---
+++ GymHelper/GymHelper/Models/BaseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace GymHelper.Models
+{
+    public class BaseModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
