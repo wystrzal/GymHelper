69e9f51da96832dd472eb1028a422555688982d1
Patryk Wyprzał <patryk836@o2.pl>
2020-12-02 14:45:46 +0100

    Add password validation extensions.

---
+++ GymHelper/GymHelper/Helpers/PasswordValidationExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymHelper.Helpers
+{
+    static class PasswordValidationExtensions
+    {
+        public static bool ContainsUpper(this string value)
+        {
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (char.IsUpper(value[i]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool ContainsDigit(this string value)
+        {
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (char.IsDigit(value[i]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
