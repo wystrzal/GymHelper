dc082da9e0463c46c3de0ab0d6b8b6633e64453d
Patryk Wyprzał <patryk836@o2.pl>
2020-12-06 16:35:48 +0100

    Add RequiredValidationBehavior.

---
+++ GymHelper/GymHelper/Helpers/RequiredValidationBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GymHelper.Helpers
+{
+    class RequiredValidationBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        public static void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            if (string.IsNullOrEmpty(args.NewTextValue))
+            {
+                ((Entry)sender).Placeholder = "To pole jest wymagane!";
+                ((Entry)sender).PlaceholderColor = Color.Red;
+            }
+        }
+    }
+}
