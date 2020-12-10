fbed793aa20d58095bc046da8de4f300eadc7940
Patryk Wyprzał <patryk836@o2.pl>
2020-12-10 18:25:46 +0100

    Remove unnecessary base model.

--- GymHelper/GymHelper/Models/Workout.cs
+++ GymHelper/GymHelper/Models/Workout.cs
@@ -8,43 +8,15 @@ using System.Text;
 
 namespace GymHelper.Models
 {
-    public class Workout : BaseModel
+    public class Workout
     {
-        private int workoutId;
-        public int WorkoutId
-        {
-            get { return workoutId; }
-            set
-            {
-                workoutId = value;
-                OnPropertyChanged("WorkoutId");
-            }
-        }
+        public int WorkoutId { get; set; }
 
         public virtual User User { get; set; }
+        public int UserId { get; set; }
 
-        private int userId;
-        public int UserId
-        {
-            get { return userId; }
-            set
-            {
-                userId = value;
-                OnPropertyChanged("UserId");
-            }
-        }
-
-        private string name;
         [Required]
-        public string Name
-        {
-            get { return name; }
-            set
-            {
-                name = value;
-                OnPropertyChanged("Name");
-            }
-        }
+        public string Name { get; set; }
 
         public DateTime Date { get; set; } = DateTime.Now;
     }
