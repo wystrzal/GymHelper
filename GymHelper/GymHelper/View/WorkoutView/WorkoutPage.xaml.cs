4b524089b2ca23864f65e47de3fa12fe06c8959f
Patryk Wyprzał <patryk836@o2.pl>
2020-12-10 18:37:04 +0100

    Add WorkoutPage.

---
+++ GymHelper/GymHelper/View/WorkoutView/WorkoutPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GymHelper.View.WorkoutView
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WorkoutPage : ContentPage
+    {
+        public WorkoutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
