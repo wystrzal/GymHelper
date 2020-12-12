a67a3f80cc61bc688969a6b60274c0190354698e
Patryk Wyprzał <patryk836@o2.pl>
2020-12-12 15:59:14 +0100

    Add NewWorkoutPageVM.

---
+++ GymHelper/GymHelper/ViewModel/WorkoutVM/NewWorkoutPageVM.cs
@@ -0,0 +1,59 @@
+﻿using GymHelper.Data;
+using GymHelper.Data.Interfaces;
+using GymHelper.Models;
+using GymHelper.ViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymHelper.ViewModel
+{
+    class NewWorkoutPageVM : BaseViewModel
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public NewWorkoutPageVM()
+        {
+            unitOfWork = App.Data.UnitOfWork;
+        }
+
+        private Workout workout;
+        public Workout Workout
+        {
+            get { return workout; }
+            set
+            {
+                workout = value;
+                OnPropertyChanged("Workout");
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                Workout = new Workout()
+                {
+                    Name = Name,
+                    UserId = App.Data.User.UserId
+                };
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public async Task AddWorkout(Workout workout)
+        {
+            await unitOfWork.Repository<Workout>().Add(workout);
+
+            if (await unitOfWork.Repository<Workout>().SaveChanges())
+            {
+                await NavigateService.NavigateBack();
+            }
+        }
+    }
+}
