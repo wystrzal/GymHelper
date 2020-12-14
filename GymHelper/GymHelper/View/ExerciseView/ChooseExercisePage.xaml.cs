0420a5aa64746c5b43fc4ae80b284429ce02bf1d
Patryk Wyprzał <patryk836@o2.pl>
2020-12-14 12:00:17 +0100

    Add ChooseExercisePage.

---
+++ GymHelper/GymHelper/View/ExerciseView/ChooseExercisePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using GymHelper.Data.Interfaces;
+using GymHelper.Models;
+using GymHelper.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GymHelper.View.ExerciseView
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChooseExercisePage : ContentPage
+    {
+        public ChooseExercisePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
