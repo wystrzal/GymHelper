296b4c9caee41c08e034e590643f913f1a332d78
Patryk Wyprzał <patryk836@o2.pl>
2020-12-15 10:44:36 +0100

    Add EditExercisePage.

---
+++ GymHelper/GymHelper/View/ExerciseView/EditExercisePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using GymHelper.Data.Interfaces;
+using GymHelper.Models;
+using GymHelper.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GymHelper.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditExercisePage : ContentPage
+    {
+        public EditExercisePage(Exercise exercise)
+        {
+            InitializeComponent();
+        }
+    }
+}
