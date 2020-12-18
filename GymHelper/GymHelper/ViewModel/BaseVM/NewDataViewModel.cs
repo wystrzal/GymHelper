6eade86792824f573d4c98227ddbd5bf32449f4c
Patryk Wyprzał <patryk836@o2.pl>
2020-12-18 11:30:48 +0100

    Add NewDataViewModel.

---
+++ GymHelper/GymHelper/ViewModel/BaseVM/NewDataViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymHelper.ViewModel.BaseVM
+{
+    public abstract class NewDataViewModel : BaseViewModel
+    {
+        public virtual async Task AddData<TEntity>(TEntity entity) where TEntity : class
+        {
+            await unitOfWork.Repository<TEntity>().Add(entity);
+            await unitOfWork.Repository<TEntity>().SaveChanges();
+            await NavigateService.NavigateBack();
+        }
+    }
+}
