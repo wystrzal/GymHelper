240c03986c1fc249b06f161ff2ecfe357bb820fb
Patryk Wyprzał <patryk836@o2.pl>
2020-12-18 11:59:30 +0100

    Add DisplayDataViewModel.

---
+++ GymHelper/GymHelper/ViewModel/BaseVM/DisplayDataViewModel.cs
@@ -0,0 +1,22 @@
+﻿using GymHelper.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymHelper.ViewModel.BaseVM
+{
+    public abstract class DisplayDataViewModel : BaseViewModel
+    {
+        public abstract Task ReadData();
+
+        public virtual async Task DeleteData<TEntity>(TEntity entity) 
+            where TEntity : class
+        {
+            await unitOfWork.Repository<TEntity>().Delete(entity);
+            await unitOfWork.Repository<TEntity>().SaveChanges();
+            await ReadData();
+        }
+    }
+}
