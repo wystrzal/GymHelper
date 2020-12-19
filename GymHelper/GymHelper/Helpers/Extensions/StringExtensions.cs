8425b1e7b27f125e1a6871ce317f3a03e033e044
Patryk Wyprzał <patryk836@o2.pl>
2020-12-19 15:27:59 +0100

    Add Capitalize string extension.

---
+++ GymHelper/GymHelper/Helpers/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymHelper.Helpers.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Capitalize(this string stringValue)
+        {
+            return char.ToUpper(stringValue[0]) + stringValue.Substring(1);
+        }
+    }
+}
