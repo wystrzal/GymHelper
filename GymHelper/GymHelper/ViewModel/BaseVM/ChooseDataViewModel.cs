4b92d735885347ecba4a1ac0bc03c82e510bf54f
Patryk Wyprzał <patryk836@o2.pl>
2020-12-20 19:41:07 +0100

    Add abstract ChooseDataViewModel and extend ChooseExercisePageVM.

---
+++ GymHelper/GymHelper/ViewModel/BaseVM/ChooseDataViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace GymHelper.ViewModel.BaseVM
+{
+    public abstract class ChooseDataViewModel<TEntity> : DisplayDataViewModel<TEntity>
+        where TEntity : class
+    {
+        public List<TEntity> SelectedData;
+        public ICommand AddSelectedDataCommand
+            => new Command(async () => await AddSelectedData());
+
+        public ChooseDataViewModel()
+        {
+            SelectedData = new List<TEntity>();
+        }
+
+        protected abstract Task AddSelectedData();
+    }
+}
--- GymHelper/GymHelper/ViewModel/ExerciseVM/ChooseExercisePageVM.cs
+++ GymHelper/GymHelper/ViewModel/ExerciseVM/ChooseExercisePageVM.cs
@@ -13,12 +13,46 @@ using Xamarin.Forms;
 
 namespace GymHelper.ViewModel
 {
-    public class ChooseExercisePageVM : DisplayDataViewModel<Exercise>
+    public class ChooseExercisePageVM : ChooseDataViewModel<Exercise>
     {
         public override ICommand NavigateToAddDataCommand 
             => new Command(async () => await NavigateService.Navigate<NewExercisePage>());
         public override ICommand NavigateToEditDataCommand 
             => new Command<Exercise>(async (exercise) => await NavigateService.Navigate<EditExercisePage>(exercise));
+
+        protected override async Task AddSelectedData()
+        {
+            List<Task> tasks = new List<Task>();
+
+            foreach (var item in SelectedData)
+            {
+                tasks.Add(AddWorkoutExercise(item));
+            }
+
+            await Task.WhenAll(tasks);
+
+            await NavigateService.NavigateBack();
+        }
+
+        private async Task AddWorkoutExercise(Exercise exercise)
+        {
+            var workoutExercise = new WorkoutExercise
+            {
+                ExerciseId = exercise.ExerciseId,
+                WorkoutId = App.Data.Workout.WorkoutId
+            };
+
+            if (!await WorkoutExerciseExist(workoutExercise))
+            {
+                await unitOfWork.Repository<WorkoutExercise>().Add(workoutExercise);
+            }
+        }
+
+        private async Task<bool> WorkoutExerciseExist(WorkoutExercise workoutExercise)
+        {
+            return await unitOfWork.Repository<WorkoutExercise>()
+                .CheckIfExistByCondition(x => x.ExerciseId == workoutExercise.ExerciseId && x.WorkoutId == workoutExercise.WorkoutId);
+        }
 
         public override async Task ReadData()
         {
