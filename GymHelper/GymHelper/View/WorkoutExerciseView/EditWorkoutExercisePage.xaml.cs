a0c94f488742ae4db1b98934a253d8d922036ed2
Patryk Wyprzał <patryk836@o2.pl>
2020-12-21 20:45:31 +0100

    Add EditWorkoutExercisePage.

---
+++ GymHelper/GymHelper/View/WorkoutExerciseView/EditWorkoutExercisePage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using GymHelper.Models;
+using GymHelper.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GymHelper.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditWorkoutExercisePage : ContentPage
+    {
+        public EditWorkoutExercisePage(WorkoutExercise workoutExercise)
+        {
+            InitializeComponent();
+            seriesEntry.Text = workoutExercise.Series == 0 ? string.Empty : workoutExercise.Series.ToString();
+            repetitionEntry.Text = workoutExercise.Repetition == 0 ? string.Empty : workoutExercise.Repetition.ToString();
+            weightEntry.Text = workoutExercise.Weight == 0 ? string.Empty : workoutExercise.Weight.ToString();
+            var viewModel = BindingContext as EditWorkoutExercisePageVM;
+            viewModel.WorkoutExercise = workoutExercise;
+        }
+    }
+}
