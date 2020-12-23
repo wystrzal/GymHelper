abba0a7b5f450635fa3a47de6e172123e322db97
Patryk Wyprzał <patryk836@o2.pl>
2020-12-23 01:16:38 +0100

    Add EditDietProductPage.

---
+++ GymHelper/GymHelper/View/DietView/EditDietProductPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using GymHelper.Models;
+using GymHelper.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GymHelper.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditDietProductPage : ContentPage
+    {
+        public EditDietProductPage(Product product)
+        {
+            InitializeComponent();
+        }
+    }
+}
