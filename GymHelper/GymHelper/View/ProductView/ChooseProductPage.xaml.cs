b7f733143568098e5e9a7906f0ab80f8ea5ded9a
Patryk Wyprzał <patryk836@o2.pl>
2020-12-24 14:25:26 +0100

    Add CheckBox_CheckedChanged event to ChooseProductPage.

--- GymHelper/GymHelper/View/ProductView/ChooseProductPage.xaml.cs
+++ GymHelper/GymHelper/View/ProductView/ChooseProductPage.xaml.cs
@@ -16,17 +16,42 @@ namespace GymHelper.View.ProductView
     public partial class ChooseProductPage : ContentPage
     {
         private readonly ChooseProductPageVM viewModel;
+        private readonly ToolbarItem addSelectedProducts;
 
         public ChooseProductPage()
         {
             InitializeComponent();
             viewModel = BindingContext as ChooseProductPageVM;
+            addSelectedProducts = new ToolbarItem();
         }
 
         protected override async void OnAppearing()
         {
             base.OnAppearing();
+
+            addSelectedProducts.Text = "Dodaj";
+            addSelectedProducts.Command = viewModel.AddSelectedDataCommand;
             await viewModel.ReadData();
+        }
+
+        private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            var checkbox = (CheckBox)sender;
+            var product = checkbox.BindingContext as Product;
+
+            if (checkbox.IsChecked)
+            {
+                ToolbarItems.Clear();
+                ToolbarItems.Add(addSelectedProducts);
+                viewModel.SelectedData.Add(product);
+            }
+
+            if (!checkbox.IsChecked)
+            {
+                ToolbarItems.Clear();
+                ToolbarItems.Add(newProduct);
+                viewModel.SelectedData.Remove(product);
+            }
         }
     }
 }
