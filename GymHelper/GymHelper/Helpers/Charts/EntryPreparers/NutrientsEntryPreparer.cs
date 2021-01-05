3eb4db002777709ad990fc31edff13a3a32899d3
Patryk Wyprzał <patryk836@o2.pl>
2021-01-05 19:01:00 +0100

    Add NutrientsEntryPreparer.

---
+++ GymHelper/GymHelper/Helpers/Charts/EntryPreparers/NutrientsEntryPreparer.cs
@@ -0,0 +1,32 @@
+﻿using GymHelper.Models;
+using Microcharts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymHelper.Helpers.Charts.EntryPreparers
+{
+    public class NutrientsEntryPreparer : ChartEntryPreparer
+    {
+        private readonly List<ChartEntry> nutrients;
+        private readonly int dietId;
+
+        public NutrientsEntryPreparer(int dietId)
+        {
+            nutrients = new List<ChartEntry>();
+            this.dietId = dietId;
+        }
+
+        public override async Task<List<ChartEntry>> PrepareChartEntry()
+        {
+            var diet = await unitOfWork.Repository<Diet>().ReadFirstByCondition(x => x.DietId == dietId);
+
+            FillChartEntryData(nutrients, diet.TotalCarbohydrates, "Węglowodany");
+            FillChartEntryData(nutrients, diet.TotalFats, "Tłuszcze");
+            FillChartEntryData(nutrients, diet.TotalProteins, "Białko");
+
+            return nutrients;
+        }
+    }
+}
