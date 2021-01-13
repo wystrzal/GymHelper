a12331450c5cf0ef7e89e924829dddcb543e2f4d
Patryk Wyprzał <patryk836@o2.pl>
2021-01-13 14:50:20 +0100

    Add abstract SearchData method and PerformSearchCommand to abstract ReadDataViewModel.

--- GymHelper/GymHelper/ViewModel/BaseVM/ReadDataViewModel.cs
+++ GymHelper/GymHelper/ViewModel/BaseVM/ReadDataViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace GymHelper.ViewModel.BaseVM
 {
@@ -10,6 +12,8 @@ namespace GymHelper.ViewModel.BaseVM
         where TEntity : class
     {
         public ObservableCollection<TEntity> Collection { get; }
+        public ICommand PerformSearchCommand
+            => new Command<string>(async (query) => await SearchData(query));
 
         public ReadDataViewModel()
         {
@@ -17,5 +21,7 @@ namespace GymHelper.ViewModel.BaseVM
         }
 
         public abstract Task ReadData();
+
+        public abstract Task SearchData(string query);
     }
 }
