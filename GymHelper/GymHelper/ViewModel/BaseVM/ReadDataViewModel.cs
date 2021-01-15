30f6c6fd5bc6676dd0eae3b84c82f23c79fa5e42
Patryk Wyprzał <patryk836@o2.pl>
2021-01-15 18:59:30 +0100

    Use FillCollection extension in ReadData method.

--- GymHelper/GymHelper/ViewModel/BaseVM/ReadDataViewModel.cs
+++ GymHelper/GymHelper/ViewModel/BaseVM/ReadDataViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GymHelper.Data.Services;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
@@ -58,7 +59,7 @@ namespace GymHelper.ViewModel.BaseVM
         public async Task ReadData()
         {
             var items = await GetData(pageIndex: 0);
-            Collection.AddRange(items);
+            Collection.FillCollection(items as List<TEntity>);
         }
     }
 }
