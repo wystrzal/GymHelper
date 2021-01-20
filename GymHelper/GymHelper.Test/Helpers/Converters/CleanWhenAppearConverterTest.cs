4706adff7400105f83764fbdf5d02f63da312134
Patryk Wyprzał <patryk836@o2.pl>
2021-01-20 17:21:16 +0100

    Test CleanWhenAppearConverter.

---
+++ GymHelper/GymHelper.Test/Helpers/Converters/CleanWhenAppearConverterTest.cs
@@ -0,0 +1,43 @@
+﻿using GymHelper.Helpers.Converters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GymHelper.Test.Helpers.Converters
+{
+    public class CleanWhenAppearConverterTest
+    {
+        private readonly CleanWhenAppearConverter converter;
+        public CleanWhenAppearConverterTest()
+        {
+            converter = new CleanWhenAppearConverter();
+        }
+
+        [Fact]
+        public void Convert_Success()
+        {
+            //Arrange
+            var testString = "test";
+
+            //Act
+            var testValue = converter.Convert(testString, typeof(string), null, null).ToString();
+
+            //Assert
+            Assert.True(string.IsNullOrWhiteSpace(testValue));
+        }
+
+        [Fact]
+        public void ConvertBack_Success()
+        {
+            //Arrange
+            var testString = "test";
+
+            //Act
+            var testValue = converter.ConvertBack(testString, typeof(string), null, null).ToString();
+
+            //Assert
+            Assert.Equal(testString, testValue);
+        }
+    }
+}
