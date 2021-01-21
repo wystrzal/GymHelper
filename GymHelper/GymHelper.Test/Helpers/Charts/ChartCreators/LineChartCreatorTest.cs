5465ecca9e2988e0bda57ac77c4e60dfc411e99d
Patryk Wyprzał <patryk836@o2.pl>
2021-01-21 16:57:52 +0100

    Test LineChartCreator.

---
+++ GymHelper/GymHelper.Test/Helpers/Charts/ChartCreators/LineChartCreatorTest.cs
@@ -0,0 +1,44 @@
+﻿using GymHelper.Data.Interfaces;
+using GymHelper.Helpers.Charts;
+using GymHelper.Helpers.Charts.EntryPreparers;
+using GymHelper.Models;
+using Microcharts;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GymHelper.Test.Helpers.Charts.ChartCreators
+{
+    public class LineChartCreatorTest
+    {
+        private readonly LineChartCreator chartCreator;
+        private readonly NutrientsEntryPreparer entryPreparer;
+        private readonly Mock<IUnitOfWork> unitOfWork;
+        private readonly Diet diet;
+
+        public LineChartCreatorTest()
+        {
+            TestHelper.PrepareUnitOfWork(out unitOfWork);
+            chartCreator = new LineChartCreator();
+            entryPreparer = new NutrientsEntryPreparer(It.IsAny<int>());
+            diet = new Diet();
+        }
+
+        [Fact]
+        public async Task CreateChart_Success()
+        {
+            //Arrange
+            unitOfWork.Setup(x => x.Repository<Diet>().ReadFirstByCondition(It.IsAny<Func<Diet, bool>>()))
+                .Returns(Task.FromResult(diet));
+
+            //Act
+            var chart = await chartCreator.CreateChart(entryPreparer);
+
+            //Assert
+            Assert.Equal(typeof(LineChart), chart.GetType());
+        }
+    }
+}
